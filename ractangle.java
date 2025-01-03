1520bc1aced6fe5fbda89cacae5f25a8c4fd3352
hetbhalala16 <hetbhalara2@gmail.com>
2025-01-03 08:22:57 +0530

    add rectangle

---
+++ ractangle.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class ractangle{
+	public static void main(String[] args){
+		Scanner sc= new Scanner(System.in);
+
+		System.out.println("enter a: ");
+		double a=sc.nextDouble();
+
+		System.out.println("enter b: ");
+		double b=sc.nextDouble();
+		
+		System.out.println("area is: "+a*b);
+
+
+	}
+}
